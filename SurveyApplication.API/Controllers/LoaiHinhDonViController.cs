c2b2727d013aeef15c3d7cf226cce711b6f08fe0
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-08-22 17:40:00 +0700

    repository CRUD

--- SurveyApplication.API/Controllers/LoaiHinhDonViController.cs
+++ SurveyApplication.API/Controllers/LoaiHinhDonViController.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SurveyApplication.Application.DTOs.LoaiHinhDonVi;
+using SurveyApplication.Application.Features.LoaiHinhDonVis.Requests.Commands;
 using SurveyApplication.Application.Features.LoaiHinhDonVis.Requests.Queries;
 
 namespace SurveyApplication.API.Controllers
@@ -18,12 +18,42 @@ namespace SurveyApplication.API.Controllers
             _mediator = mediator;
         }
 
-        // GET: api/<LoaiHinhDonViController>
-        [HttpGet("GetByLoaiHinhDonVi")]
-        public async Task<ActionResult<List<LoaiHinhDonViDto>>> GetByLoaiHinhDonVi(string maLoaiHinh)
+        [HttpGet("GetByLoaiHinhDonVi/{id}")]
+        public async Task<ActionResult<List<LoaiHinhDonViDto>>> GetByLoaiHinhDonVi(string id)
         {
-            var leaveAllocations = await _mediator.Send(new GetLoaiHinhDonViDetailRequest() { MaLoaiHinh = maLoaiHinh });
+            var leaveAllocations = await _mediator.Send(new GetLoaiHinhDonViDetailRequest() { MaLoaiHinh = id });
             return Ok(leaveAllocations);
+        }
+
+        [HttpGet("GetAllLoaiHinhDonVi")]
+        public async Task<ActionResult<List<LoaiHinhDonViDto>>> GetAllLoaiHinhDonVi()
+        {
+            var leaveAllocations = await _mediator.Send(new GetLoaiHinhDonViListRequest());
+            return Ok(leaveAllocations);
+        }
+
+        [HttpPost("CreateLoaiHinhDonVi")]
+        public async Task<ActionResult<LoaiHinhDonViDto>> CreateLoaiHinhDonVi([FromBody] LoaiHinhDonViDto obj)
+        {
+            var command = new CreateLoaiHinhDonViCommand { LoaiHinhDonViDto = obj };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        [HttpPost("UpdateLoaiHinhDonVi")]
+        public async Task<ActionResult<LoaiHinhDonViDto>> UpdateLoaiHinhDonVi([FromBody] LoaiHinhDonViDto obj)
+        {
+            var command = new UpdateLoaiHinhDonViCommand { LoaiHinhDonViDto = obj };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpGet("DeleteLoaiHinhDonVi/{id}")]
+        public async Task<ActionResult<List<LoaiHinhDonViDto>>> DeleteLoaiHinhDonVi(string id)
+        {
+            var command = new DeleteLoaiHinhDonViCommand { MaLoaiHinh = id };
+            await _mediator.Send(command);
+            return NoContent();
         }
     }
 }
