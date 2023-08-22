0881c0052754fc6641150800ce7b9dd4c1f73fb8
DESKTOP-MASN9OD\nguye <toannck32@wru.vn>
2023-08-22 14:33:45 +0700

    update Dto

---
+++ SurveyApplication.Application/DTOs/LoaiHinhDonVi/LoaiHinhDonViDto.cs
@@ -0,0 +1,16 @@
+﻿using SurveyApplication.Application.DTOs.Common;
+
+namespace SurveyApplication.Application.DTOs.LoaiHinhDonVi
+{
+    public partial class LoaiHinhDonViDto : BaseDto
+    {
+        public string Maloaihinh { get; set; } = null!;
+        public string? Tenloaihinh { get; set; }
+        public string? Mota { get; set; }
+        public int? ActiveFlag { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? Created { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? Modified { get; set; }
+    }
+}
