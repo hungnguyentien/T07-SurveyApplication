758a1eaa87c98f8a266e0a0bbd5f8bca30b14b9c
DESKTOP-MASN9OD\nguye <toannck32@wru.vn>
2023-08-23 14:55:57 +0700

    update Migrations

---
+++ SurveyApplication.Domain/DapAn.cs
@@ -0,0 +1,18 @@
+﻿using SurveyApplication.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Domain
+{
+    public class DapAn : BaseDomainEntity
+    {
+        [Required]
+        public string MaDapAn { get; set; }
+        public string NoiDung { get; set; }
+        public int IdCauHoi { get; set; }
+    }
+}
