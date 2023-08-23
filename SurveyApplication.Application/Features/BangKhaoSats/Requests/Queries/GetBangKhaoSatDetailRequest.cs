3b5e2e302a195ce1717dc89014134ace4387189a
DUC THANH <phithanh365@gmail.com>
2023-08-23 16:09:49 +0700

    update reqquire

---
+++ SurveyApplication.Application/Features/BangKhaoSats/Requests/Queries/GetBangKhaoSatDetailRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Application.Features.BangKhaoSats.Requests.Queries
+{
+    internal class GetBangKhaoSatDetailRequest
+    {
+    }
+}
