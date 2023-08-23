cb3789ba27c207d9813270107cc7f9eb5c8f682d
THANH DUC <phithanh365@gmail.com>
2023-08-23 08:42:07 +0700

    create table

--- SurveyApplication.Persistence/SurveyApplicationDbContext.cs
+++ SurveyApplication.Persistence/SurveyApplicationDbContext.cs
@@ -36,5 +36,18 @@ namespace SurveyApplication.Persistence
         }
 
         public DbSet<LoaiHinhDonVi> LoaiHinhDonVis { get; set; }
+        public DbSet<LoaiCauHoi> LoaiCauHoiVis { get; set; }
+        public DbSet<LinhVucHoatDong>LinhVucHoatDongs { get; set; }
+        public DbSet<GuiEmail>GuiEmails { get; set; }
+        public DbSet<DotKhaoSat> DotKhaoSats { get; set; }
+        public DbSet<CauHoi> CauHois { get; set; }
+        public DbSet<BangKhaoSat> BangKhaoSats { get;set; }
+        public DbSet<NguoiDaiDien>NguoiDaiDiens { get; set; }
+        public DbSet<NguoiDung>NguoiDungs { get; set; }
+        public DbSet<NguoiDungVaiTro> NguoiDungVaiTros { get; set; }
+        public DbSet<Quyen> Quyens { get; set; }
+        public DbSet<VaiTro> VaiTros { get; set; }
+        public DbSet<VaiTroQuyen> VaiTroQuyens { get; set; }
+        public DbSet<DonVi>DonVis { get;set; }
     }
 }
