b9cd04d0e1692e4e92f51bccf028e60e6f3efa78
toan nguyen <toannck32@wru.vn>
2023-08-24 20:27:21 +0700

    update

---
+++ SurveyApplication.Application/Features/LoaiHinhDonVis/Handlers/Queries/GetLoaiHinhDonViSearchRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using SurveyApplication.Application.Contracts.Persistence;
+using SurveyApplication.Application.DTOs.LoaiHinhDonVi;
+using SurveyApplication.Application.Features.LoaiHinhDonVis.Requests.Queries;
+
+namespace SurveyApplication.Application.Features.LoaiHinhDonVis.Handlers.Queries
+{
+    public class GetLoaiHinhDonViSearchRequestHandler : IRequestHandler<GetLoaiHinhDonViSearchRequest, List<LoaiHinhDonViDto>>
+    {
+        private readonly ILoaiHinhDonViRepository _loaiHinhDonViRepository;
+        private readonly IMapper _mapper;
+
+        public GetLoaiHinhDonViSearchRequestHandler(ILoaiHinhDonViRepository loaiHinhDonViRepository, IMapper mapper)
+        {
+            _loaiHinhDonViRepository = loaiHinhDonViRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LoaiHinhDonViDto>> Handle(GetLoaiHinhDonViSearchRequest request, CancellationToken cancellationToken)
+        {
+            var loaiHinhDonVis = await _loaiHinhDonViRepository.GetByConditions(request.PageIndex, request.PageSize, x => string.IsNullOrEmpty(request.Keyword) || !string.IsNullOrEmpty(x.TenLoaiHinh) && x.TenLoaiHinh.Contains(request.Keyword));
+            return _mapper.Map<List<LoaiHinhDonViDto>>(loaiHinhDonVis);
+        }
+    }
+}
