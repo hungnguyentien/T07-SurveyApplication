a9fed72df8b6e906c66ab7f29c2d91c49098d952
DESKTOP-MASN9OD\nguye <toannck32@wru.vn>
2023-08-25 14:59:56 +0700

    update Migration UpdateTableHang

---
+++ SurveyApplication.API/Controllers/CauHoiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyApplication.API.Controllers
+{
+    public class CauHoiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
