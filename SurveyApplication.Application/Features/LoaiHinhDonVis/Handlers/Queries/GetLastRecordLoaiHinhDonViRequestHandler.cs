f50a0055f6ee3264b231cccbf496085bdf08a755
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-08-25 13:42:58 +0700

    3

---
+++ SurveyApplication.Application/Features/LoaiHinhDonVis/Handlers/Queries/GetLastRecordLoaiHinhDonViRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using SurveyApplication.Application.Contracts.Persistence;
+using SurveyApplication.Application.DTOs.LoaiHinhDonVi;
+using SurveyApplication.Application.Features.LoaiHinhDonVis.Requests.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Application.Features.LoaiHinhDonVis.Handlers.Queries
+{
+    public class GetLastRecordLoaiHinhDonViRequestHandler : IRequestHandler<GetLastRecordLoaiHinhDonViRequest, string>
+    {
+        private readonly ILoaiHinhDonViRepository _LoaiHinhDonViRepository;
+        private readonly IMapper _mapper;
+
+        public GetLastRecordLoaiHinhDonViRequestHandler(ILoaiHinhDonViRepository LoaiHinhDonViRepository, IMapper mapper)
+        {
+            _LoaiHinhDonViRepository = LoaiHinhDonViRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<string> Handle(GetLastRecordLoaiHinhDonViRequest request, CancellationToken cancellationToken)
+        {
+            var LoaiHinhDonVis = await _LoaiHinhDonViRepository.GetLastRecordByMaLoaiHinh();
+            return LoaiHinhDonVis;
+        }
+    }
+}
