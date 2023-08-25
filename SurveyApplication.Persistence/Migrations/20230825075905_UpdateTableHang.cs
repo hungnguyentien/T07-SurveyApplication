e2958b93c68aeaba7c71ed2183fe777ff5abd683
DESKTOP-MASN9OD\nguye <toannck32@wru.vn>
2023-08-25 15:20:09 +0700

    update Migrations

---
+++ SurveyApplication.Persistence/Migrations/20230825075905_UpdateTableHang.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyApplication.Persistence.Migrations
+{
+    public partial class UpdateTableHang : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NoiDung",
+                table: "Hang",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOther",
+                table: "Hang",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LabelCauTraLoi",
+                table: "Hang",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOther",
+                table: "Hang");
+
+            migrationBuilder.DropColumn(
+                name: "LabelCauTraLoi",
+                table: "Hang");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NoiDung",
+                table: "Hang",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
