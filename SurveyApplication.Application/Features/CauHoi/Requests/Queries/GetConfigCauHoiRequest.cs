bd16c76a93aa1bdba1bf46a94e3f8f39801d45ca
DESKTOP-MASN9OD\nguye <toannck32@wru.vn>
2023-08-28 08:07:01 +0700

    update phieu khao sat

---
+++ SurveyApplication.Application/Features/CauHoi/Requests/Queries/GetConfigCauHoiRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SurveyApplication.Application.DTOs.BangKhaoSat;
+using SurveyApplication.Application.DTOs.CauHoi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyApplication.Application.Features.CauHoi.Requests.Queries
+{
+    public class GetConfigCauHoiRequest:IRequest<List<CauHoiDto>>
+    {
+        public int IdBangKhaoSat { get; set; }
+    }
+}
