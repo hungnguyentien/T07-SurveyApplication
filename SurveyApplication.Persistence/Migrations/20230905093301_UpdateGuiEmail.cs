e5bc3644dd6a98fe25b36beb42cb85fffd8a5108
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-09-05 17:38:16 +0700

    update migration

---
+++ SurveyApplication.Persistence/Migrations/20230905093301_UpdateGuiEmail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyApplication.Persistence.Migrations
+{
+    public partial class UpdateGuiEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ThoiGian",
+                table: "GuiEmail",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "TrangThai",
+                table: "GuiEmail",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThoiGian",
+                table: "GuiEmail");
+
+            migrationBuilder.DropColumn(
+                name: "TrangThai",
+                table: "GuiEmail");
+        }
+    }
+}
