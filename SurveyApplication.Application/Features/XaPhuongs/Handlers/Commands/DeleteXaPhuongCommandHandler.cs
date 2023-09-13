c41e44b8e3ea82656dd5c266007574eaaf893a41
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-09-13 13:36:25 +0700

    update quanhuyentinhxa

--- SurveyApplication.Application/Features/XaPhuongs/Handlers/Commands/DeleteXaPhuongCommandHandler.cs
+++ SurveyApplication.Application/Features/XaPhuongs/Handlers/Commands/DeleteXaPhuongCommandHandler.cs
@@ -2,12 +2,12 @@
 using MediatR;
 using SurveyApplication.Application.Exceptions;
 using SurveyApplication.Application.Features.XaPhuongs.Requests.Commands;
-using SurveyApplication.Domain;
+using SurveyApplication.Domain.Common.Responses;
 using SurveyApplication.Domain.Interfaces.Persistence;
 
 namespace SurveyApplication.Application.Features.XaPhuongs.Handlers.Commands
 {
-    public class DeleteXaPhuongCommandHandler : BaseMasterFeatures, IRequestHandler<DeleteXaPhuongCommand>
+    public class DeleteXaPhuongCommandHandler : BaseMasterFeatures, IRequestHandler<DeleteXaPhuongCommand, BaseCommandResponse>
     {
         private readonly IMapper _mapper;
 
@@ -16,16 +16,20 @@ namespace SurveyApplication.Application.Features.XaPhuongs.Handlers.Commands
             _mapper = mapper;
         }
 
-        public async Task<Unit> Handle(DeleteXaPhuongCommand request, CancellationToken cancellationToken)
+        public async Task<BaseCommandResponse> Handle(DeleteXaPhuongCommand request, CancellationToken cancellationToken)
         {
-            var XaPhuongRepository = await _surveyRepo.XaPhuong.GetById(request.Id);
-            if (XaPhuongRepository == null)
+            var lstXaPhuong = await _surveyRepo.XaPhuong.GetByIds(x => request.Ids.Contains(x.Id));
+            if (lstXaPhuong == null || lstXaPhuong.Count == 0)
             {
-                throw new NotFoundException(nameof(XaPhuong), request.Id);
+                //throw new NotFoundException(nameof(XaPhuong), request.Ids);
             }
-            await _surveyRepo.XaPhuong.Delete(XaPhuongRepository);
+            foreach (var item in lstXaPhuong)
+            {
+                item.Deleted = true;
+            }
+            //await _surveyRepo.XaPhuong.DeleteAsync(lstXaPhuong);
             await _surveyRepo.SaveAync();
-            return Unit.Value;
+            return new BaseCommandResponse("Xóa thành công!");
         }
     }
 }
