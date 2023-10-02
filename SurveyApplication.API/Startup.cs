1b56a68054cb4dc3ed95e24b9688bc51abc51dad
toan nguyen <toannck32@wru.vn>
2023-10-03 00:04:53 +0700

    update ExternalDocs

--- SurveyApplication.API/Startup.cs
+++ SurveyApplication.API/Startup.cs
@@ -52,7 +52,17 @@ public class Startup
 
         app.UseAuthentication();
 
-        app.UseSwagger();
+        app.UseSwagger(c =>
+        {
+            c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+            {
+                swaggerDoc.ExternalDocs = new OpenApiExternalDocs
+                {
+                    Description = "Survey Management Api",
+                    Url = new Uri($"{httpReq.Scheme}://{httpReq.Host.Value}")
+                };
+            });
+        });
 
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SurveyManagement.Api v1"));
 
