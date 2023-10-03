369aface21d806897a11d552e6632d1df1b223e9
dinhhoanganh2020@gmail.com <dinhhoanganh2020@gmail.com>
2023-10-03 17:32:03 +0700

    update account

--- SurveyApplication.Application/DTOs/Account/UpdateAccountDto.cs
+++ SurveyApplication.Application/DTOs/Account/UpdateAccountDto.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using SurveyApplication.Application.DTOs.Role;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,8 @@ namespace SurveyApplication.Application.DTOs.Account
         public string? Image { get; set; }
 
         public IFormFile? Img { get; set; }
+        public List<MatrixPermission>? MatrixPermission { get; set; }
+
+        public List<string>? LstRoleName { get; set; }
     }
 }
