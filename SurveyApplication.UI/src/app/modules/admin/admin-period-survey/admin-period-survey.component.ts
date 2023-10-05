a3eecd469bb79a7e284f296e16e115788eba1cca
DUC THANH <phithanh365@gmail.com>
2023-10-05 16:23:54 +0700

    update lọc bảng kháo sát,đợt khảo sát

--- SurveyApplication.UI/src/app/modules/admin/admin-period-survey/admin-period-survey.component.ts
+++ SurveyApplication.UI/src/app/modules/admin/admin-period-survey/admin-period-survey.component.ts
@@ -138,25 +138,72 @@ export class AdminPeriodSurveyComponent {
       },
     });
   }
+  // loadListLazy = (event: any) => {
+  //   this.loading = true;
+  //   let pageSize = event.rows;
+  //   let pageIndex = event.first / pageSize + 1;
+  //   this.paging = {
+  //     pageIndex: pageIndex,
+  //     pageSize: pageSize,
+  //     keyword: '',
+  //     orderBy: event.sortField
+  //       ? `${event.sortField} ${event.sortOrder === 1 ? 'asc' : 'desc'}`
+  //       : '',
+  //   };
+  //   this.PeriodSurveyService.getByCondition(this.paging).subscribe({
+  //     next: (res) => {
+  //       this.datas = res.data;
+  //       this.dataTotalRecords = res.totalFilter;
+  //     },
+  //     error: (e) => {
+  //       Utils.messageError(this.messageService, e.message);
+  //       this.loading = false;
+  //     },
+  //     complete: () => {
+  //       this.loading = false;
+  //     },
+  //   });
+  // };
+  
   loadListLazy = (event: any) => {
+    debugger;
     this.loading = true;
     let pageSize = event.rows;
     let pageIndex = event.first / pageSize + 1;
+  
+    // Khởi tạo keyword là một chuỗi trống
+    let keyword = this.keyWord|| '';
+  
+    // Lặp qua các filter để xây dựng keyword
+    for (const field in event.filters) {
+      if (event.filters.hasOwnProperty(field)) {
+        const filterValue = event.filters[field][0].value;
+        if (filterValue != null) {
+          // Nếu filterValue không null, thì thêm vào keyword với điều kiện phù hợp
+          keyword += `${filterValue}`;
+        }
+      }
+    }
+    // // Xóa khoảng trắng và "and" cuối cùng khỏi keyword
+    // keyword = keyword.trim();
+    // if (keyword.endsWith('and')) {
+    //   keyword = keyword.slice(0, -3);
+    // }  
     this.paging = {
       pageIndex: pageIndex,
       pageSize: pageSize,
-      keyword: '',
+      keyword:keyword, // Sử dụng keyword mới xây dựng từ các filter
       orderBy: event.sortField
         ? `${event.sortField} ${event.sortOrder === 1 ? 'asc' : 'desc'}`
         : '',
     };
+  
     this.PeriodSurveyService.getByCondition(this.paging).subscribe({
       next: (res) => {
         this.datas = res.data;
         this.dataTotalRecords = res.totalFilter;
       },
       error: (e) => {
-        Utils.messageError(this.messageService, e.message);
         this.loading = false;
       },
       complete: () => {
@@ -164,6 +211,7 @@ export class AdminPeriodSurveyComponent {
       },
     });
   };
+  
 
   onSubmitSearch = () => {
     this.paging.keyword = this.keyWord;
