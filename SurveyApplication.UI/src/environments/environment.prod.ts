142882e24bb7d09997aaa6e99b440d829a7b81d1
ToanNck <toannck32@wru.vn>
2023-11-30 15:57:31 +0700

    update gưi mail

--- SurveyApplication.UI/src/environments/environment.prod.ts
+++ SurveyApplication.UI/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://kstt-api.dttt.vn/api',
-  apiIp: 'https://ipinfo.io/json?token=a4f2fcddf28a9a',
+  apiIp: 'https://ipinfo.io/json?token=fd8662b7f72701',
   secretKey: 'T0aNnCk2o2e',
   fakeApiUrl: 'http://10.0.70.17:3001/api',
   apiUrlImage: 'https://kstt-api.dttt.vn/Images/',
