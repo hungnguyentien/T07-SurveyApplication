267cbfdcba64236f79551890defcdb91d5df3b16
ADMIN\Admin <linhdo05022k2@gmail.com>
2023-12-01 11:16:29 +0700

    update_docker

--- SurveyApplication.UI/src/environments/environment.docker.ts
+++ SurveyApplication.UI/src/environments/environment.docker.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrlImage:'https://localhost:8000/Images/',
-  apiUrl: 'https://localhost:8000/api',
+  apiUrlImage:'http://10.0.68.96/Images/',
+  apiUrl: 'http://10.0.68.96/api',
   apiIp: 'https://ipinfo.io/json?token=a4f2fcddf28a9a',
   secretKey: 'T0aNnCk2o2e',
   fakeApiUrl: 'http://localhost:3001/api',
